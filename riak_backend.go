0cdc3a25108eb186ba349cae10226db1f567c7f7
UENISHI Kota <kuenishi@gmail.com>
2013-09-01 02:06:43 +0900

    get/put/delete to Riak

--- riak_backend.go
+++ riak_backend.go
@@ -1,6 +1,7 @@
 package dicorn
 
 import (
+	"fmt"
 	"strings"
 	"github.com/mrb/riakpbc"
 )
@@ -14,6 +15,11 @@ type RiakBackend struct {
 func NewRiakBackend(hosts string) *RiakBackend {
 	h := strings.Split(hosts, ",")
 	// TODO: check riak existence here
+	c := riakpbc.NewClient(h)
+	if c.Dial() == nil {
+		fmt.Printf("riak is ok: %v\n", c)
+	}
+	c.Close()
 	return &RiakBackend{hosts: h}
 }
 
@@ -22,21 +28,27 @@ func (rb *RiakBackend) Init() {
 
 func (rb *RiakBackend) Set(key string, flag, expire, size int, value []byte) error {
 	c := rb.connect()
+	//fmt.Printf("%s %s %v %v\n", BUCKET, key, string(value), c)
 	_, err := c.StoreObject(BUCKET, key, value)
-	c.Close()
+	//fmt.Printf(" => %v / %v\n", r, err)
+	rb.disconnect(c)
 	return err
 }
 func (rb *RiakBackend) Get(key string) ([]byte, error) {
 	c := rb.connect()
 	obj, err := c.FetchObject(BUCKET, key)
-	c.Close()
+	rb.disconnect(c)
+	//fmt.Printf(" => %v / %v\n", obj, err)
 	if err != nil {
 		return nil, &NotFoundError{}
 	}
 	return (obj.GetContent()[0].GetValue()), nil
 }
 func (rb *RiakBackend) Delete(key string) error {
-	return &NotFoundError{}
+	c := rb.connect()
+	_, err := c.DeleteObject(BUCKET, key)
+	rb.disconnect(c)
+	return err
 }
 func (rb *RiakBackend) Incr(key string, value int) (int, error) {
 	return 0, &NotFoundError{}
@@ -46,5 +58,10 @@ func (rb *RiakBackend) Decr(key string, value int) (int, error) {
 }
 
 func (rb *RiakBackend) connect() *riakpbc.Client {
-	return riakpbc.NewClient(rb.hosts)
+	c := riakpbc.NewClient(rb.hosts)
+	c.Dial()
+	return c
+}
+func (rb *RiakBackend) disconnect(c *riakpbc.Client) {
+	c.Close()
 }
